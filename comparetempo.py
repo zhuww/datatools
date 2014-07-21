47b8c94de7b694015aa97e7b33f7fd5169037cef
Weiwei Zhu <zhuwwpku@gmail.com>
2014-07-21 16:29:18 -0700

    add the comparetempo.py utility code for comparing tempo parfiles.

---
+++ comparetempo.py
@@ -0,0 +1,80 @@
+from datatools.tempo import *
+import sys,os
+
+import argparse
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='comparetempo.py')
+    parser.add_argument('par1', help='parfile 1')
+    parser.add_argument('par2', help='parfile 2')
+    parser.add_argument('--withdmx', help='withdmx', type=bool, default=False)
+    parser.add_argument('--plot', help='makeplot', type=bool, default=False)
+    values = parser.parse_args(sys.argv[1:])
+
+    f1, f2 = values.par1, values.par2
+    withdmx = values.withdmx
+    makeplot = values.plot
+    #print withdmx, type(withdmx)
+
+    m1 = model(f1)
+    m2 = model(f2)
+
+    par1 = [p for p in m1.manifest if not p.startswith('DMX_') and p in m1.parameters]
+    dmx1 = [p for p in m1.manifest if p.startswith('DMX_') and p in m1.parameters]
+    par2 = [p for p in m2.manifest if not p.startswith('DMX_') and p in m2.parameters]
+    dmx2 = [p for p in m2.manifest if p.startswith('DMX_') and p in m2.parameters]
+
+    par = [p for p in par1 if p in par2]
+    dmx = [p for p in dmx1 if p in dmx2]
+    if withdmx:
+        par = par + dmx
+
+    print 'parname:| par1 \t|\t par2 \t|\t abs(par1-par2)/max(sigma1, sigma2)'
+    value1 = []
+    value2 = []
+    error1 = []
+    error2 = []
+    deviat = []
+    redlist = []
+    i = 0
+    for p in par:
+        if p in ['RAJ', 'DECJ']:
+            val1 = float(m1.__dict__[p][0].split(':')[-1])
+            val2 = float(m2.__dict__[p][0].split(':')[-1])
+            err1 = float(m1.__dict__[p][1])
+            err2 = float(m2.__dict__[p][1])
+        else:
+            val1 = float(m1.__dict__[p][0])
+            val2 = float(m2.__dict__[p][0])
+            err1 = float(m1.__dict__[p][1])
+            err2 = float(m2.__dict__[p][1])
+        dev = abs(val1-val2)/max((err1, err2))
+        value1.append(val1)
+        value2.append(val2)
+        error1.append(err1)
+        error2.append(err2)
+        deviat.append(dev)
+        if dev > 3:
+            print p, ':', m1.__dict__[p][0], '\t|\t',  m2.__dict__[p][0], '\t|\t', dev
+            redlist.append(i)
+        i+=1
+
+    print par
+    if makeplot:
+        #from pylab import *
+        import matplotlib.pyplot as plt
+        fig, ax1 = plt.subplots()#figsize=(10,6))
+        fig.canvas.set_window_title('compare %s %s' % ( f1, f2))
+        ax1.yaxis.grid(True, linestyle='-', which='major', color='lightblue')
+        xticks = plt.setp(ax1, xticks=range(len(par)))
+        xtickNames = plt.setp(ax1, xticklabels=par)
+        #xtickNames = plt.setp(ax1, xticklabels=[('%s' % s) for s in range(len(par))])
+        plt.setp(xtickNames, rotation=45, fontsize=8)
+        xvals = np.array(range(len(par)))
+        numpars = len(par)
+        ax1.set_xlim(0.5, numpars+0.5)
+        ax1.errorbar(xvals, (np.array(value2) - np.array(value1))/np.array(error1), yerr=np.array(error2)/np.array(error1), fmt='r.')
+        ax1.errorbar(xvals, (np.array(value1) - np.array(value1))/np.array(error1), yerr=np.ones(len(error1)), fmt='b.')
+        ax1.set_xlim(0, len(par))
+        #print ax1.get_ticks(xvals)
+        #print ax1.get_ticklabels(xvals)
+        plt.show()
