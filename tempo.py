ae48b630c3286d285bc972efea72625e5b756986
Weiwei Zhu <zhuwwpku@gmail.com>
2014-11-11 12:42:11 -0800

    commit.

--- tempo.py
+++ tempo.py
@@ -11,8 +11,8 @@ from numpy.core.records import fromfile
 #from numpy import float64 as __Decimal
 def initmatplotlib():
     import matplotlib as mpl 
-    #fig_width_pt = 513.17  # Get this from LaTeX using \showthe\columnwidth
-    fig_width_pt = 246.5  # Get this from LaTeX using \showthe\columnwidth
+    fig_width_pt = 513.17  # Get this from LaTeX using \showthe\columnwidth
+    #fig_width_pt = 246.5  # Get this from LaTeX using \showthe\columnwidth
     inches_per_pt = 1.0/72.27               # Convert pt to inches
     golden_mean = (sqrt(5)-1.0)/2.0         # Aesthetic ratio
     fig_width = fig_width_pt*inches_per_pt  # width in inches
